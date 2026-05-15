2974859aee1447ede7af8aef225566a226c9e104
202510703DSAabdalla <karlos4ke@gmail.com>
2026-05-15 03:51:21 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+
+
+ string   name ;
+cout << "enter user name";
+cin >> name;
+
+
+cout <<"congratulations :" << name<< endl;
+
+return 0;
+}
